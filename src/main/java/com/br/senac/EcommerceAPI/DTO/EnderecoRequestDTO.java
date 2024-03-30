63e9874bf7d8403f54f20ed02c10793a7aead95d
jcavalcantee <jcavalcantes@gmail.com>
2024-03-30 18:01:40 -0300

    classes para lidar com o cadastro de endereços

---
+++ src/main/java/com/br/senac/EcommerceAPI/DTO/EnderecoRequestDTO.java
@@ -0,0 +1,15 @@
+package com.br.senac.EcommerceAPI.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter
+public class EnderecoRequestDTO {
+
+    private String cep;
+    private String numero;
+}
