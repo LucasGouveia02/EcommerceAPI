80fe88e47d98f089c0fa12f3b8d3e8bd61131069
jcavalcantee <jcavalcantes@gmail.com>
2024-04-30 19:41:16 -0300

    pegando endereço do usuario autenticado

--- src/main/java/com/br/senac/EcommerceAPI/Repositories/EnderecoRepository.java
+++ src/main/java/com/br/senac/EcommerceAPI/Repositories/EnderecoRepository.java
@@ -1,8 +1,11 @@
 package com.br.senac.EcommerceAPI.Repositories;
 
 import com.br.senac.EcommerceAPI.Models.EnderecoModel;
+import com.br.senac.EcommerceAPI.Models.EnderecoUsuario;
+import com.br.senac.EcommerceAPI.Models.UsuarioModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -10,4 +13,9 @@ public interface EnderecoRepository extends JpaRepository<EnderecoModel, Long> {
 
     @Query("SELECT u FROM EnderecoModel u WHERE u.cep = ?1")
     EnderecoModel buscaPorCep(String cep);
+
+    @Query("SELECT e " +
+            "FROM EnderecoUsuario e " +
+            "WHERE e.id.usuarioId = :usuarioId")
+    EnderecoUsuario retornoEnderecoPorUsuario(@Param("usuarioId") UsuarioModel id);
 }
